23e23a27308fbb8c7ef4329cefd719e5b2050b8a
octo <dlqhdgkr456@gmail.com>
2021-09-14 22:05:15 +0900

    9.9 Deleting To Dos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { useReducer, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const initialState = {
   toDos: []
 }
 
 const ADD = "add";
+const DELETE = "delete";
 
 // return 하는 object가 state에 들어가게 될 object (replace)
 const reducer = (state, action) => {
   switch (action.type) {
     case ADD:
-      return { toDos: [...state.toDos, { text: action.payload }] };
+      return { toDos: [...state.toDos, { text: action.payload, id: uuidv4() }] };
+    case DELETE:
+      return { toDos: state.toDos.filter(toDo => toDo.id !== action.payload) };
     default:
       throw new Error();
   }
@@ -24,7 +28,7 @@ const App = () => {
     dispatch({type: ADD, payload: newToDo});
   };
   const onChange = event => {
-    const { target : { value }} = event;
+    const { target: { value }} = event;
     setNewToDo(value);
   };
   return (
@@ -35,7 +39,11 @@ const App = () => {
       </form>
       <ul>
         <h2>To Dos</h2>
-        {state.toDos.map((toDo, index) => <li key={index}>{toDo.text}</li>)}
+        {state.toDos.map((toDo) => (
+        <li key={toDo.id}>
+          <span>{toDo.text}</span>
+          <button onClick={() => dispatch({type: DELETE, payload: toDo.id})}>❌</button>
+        </li>))}
       </ul>
     </>
   );
